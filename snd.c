448d5e9c50c65c35a8682fb3e5a52ec54023cfd0
Ivan (w23) Avdeev <me@w23.ru>
2011-09-04 00:21:44 +0700

    sound test in high level language with objects and garbage collection: c

---
+++ snd.c
@@ -0,0 +1,59 @@
+// cc -I/usr/include/SDL snd.c -lSDL -o s && ./s
+
+#include <math.h>
+#include <SDL.h>
+
+struct {
+	int ptr, left;
+	float p, dp;
+	float	e, de;
+} wave;
+
+float ntf;
+
+int seq[] = {1, 6, 8, 11, 1, 6, 8, 15};
+int seq_len[] = {2, 4, 4,  2, 2, 4, 4,  2};
+
+void callback(void *userdata, Uint8 *stream, int len)
+{
+	short* p = (short*)stream;
+	len/=2;
+	for(;len > 0; len--)
+	{
+		if (wave.left == 0)
+		{
+			wave.left = 44100. / seq_len[wave.ptr];
+			wave.dp = 2. * M_PI* 440. * pow(ntf, seq[wave.ptr]) / 44100.;
+			wave.de = (M_PI / 0.06) / 44100.;
+			wave.e = 0.;
+			wave.ptr++;
+			wave.ptr&=7;
+		}
+
+		wave.left--;
+		wave.p += wave.dp;
+		wave.e += wave.de;
+		if (wave.e >= M_PI) wave.de = 0.;
+		*p = 32767. * sin(wave.e) * sin(wave.p);
+		p++;
+	}
+}
+
+SDL_AudioSpec as = {44100, 0x8010, 1, 0, 0, 0, 0, callback};
+
+void main(void)
+{
+	SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO);
+	SDL_SetVideoMode(640,480,32,0);
+	SDL_OpenAudio(&as, 0);
+	memset(&wave,0,sizeof(wave));
+	ntf = pow(2., 1./12.);
+	SDL_PauseAudio(0);
+	for(;;)
+	{
+		SDL_Event e;
+		SDL_WaitEvent(&e);
+		if (e.type == 2) break;
+	}
+	SDL_Quit();
+}
